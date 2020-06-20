0ce3ba598fb1e6aac993fbcd1176994469d12a7b
Nila <sangeetha9223@gmail.com>
2020-06-20 23:45:58 +0530

    Middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { indexPage, indexnewPage, messagesPage } from '../controllers';
+import { indexPage, indexnewPage, messagesPage, addMessage  } from '../controllers';
+import { modifyMessage } from '../middleware';
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/new', indexnewPage);
-indexRouter.get('/messages', messagesPage)
+indexRouter.get('/messages', messagesPage);
+indexRouter.post('/messages', modifyMessage, addMessage);
 
 export default indexRouter;
