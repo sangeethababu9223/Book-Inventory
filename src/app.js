e95826c0108db7173636ff5f2b2521b0d2c510c1
Nila <sangeetha9223@gmail.com>
2020-06-20 23:51:34 +0530

    Error handling

--- src/app.js
+++ src/app.js
@@ -10,5 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/v1', indexRouter);
 // app.use('/new', indexRouter);
-
+app.use((err, req, res, next) => {
+    res.status(400).json({ error: err.stack });
+});
 export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { indexPage, indexnewPage, messagesPage, addMessage  } from '../controllers';
-import { modifyMessage } from '../middleware';
+import { modifyMessage, performAsyncAction } from '../middleware';
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/new', indexnewPage);
 indexRouter.get('/messages', messagesPage);
-indexRouter.post('/messages', modifyMessage, addMessage);
+indexRouter.post('/messages', modifyMessage, performAsyncAction, addMessage);
 
 export default indexRouter;
